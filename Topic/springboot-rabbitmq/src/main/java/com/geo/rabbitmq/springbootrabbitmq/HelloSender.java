65c264006f50228f948db36a9fa646d3e8e845d3
lihaiyang2046 <1160362008@qq.com>
2018-12-25 16:34:24 +0800

    新增rabbitmq中topic模式

---
+++ Topic/springboot-rabbitmq/src/main/java/com/geo/rabbitmq/springbootrabbitmq/HelloSender.java
@@ -0,0 +1,19 @@
+package com.geo.rabbitmq.springbootrabbitmq;
+
+import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class HelloSender {
+    @Autowired
+    private AmqpTemplate template;
+
+    @RequestMapping("/send")
+    public void send() {
+        template.convertAndSend("exchange", "topic.messages", "hello,rabbit");
+    }
+}
---
+++ Topic/springboot-rabbitmq2/src/main/java/com/geo/rabbitmq/springbootrabbitmq/HelloReceive.java
@@ -0,0 +1,16 @@
+package com.geo.rabbitmq.springbootrabbitmq;
+
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class HelloReceive {
+    @RabbitListener(queues="topic.message")    //监听器监听指定的Queue
+    public void process1(String str) {
+        System.out.println("message:"+str);
+    }
+    @RabbitListener(queues="topic.messages")    //监听器监听指定的Queue
+    public void process2(String str) {
+        System.out.println("messages:"+str);
+    }
+}
